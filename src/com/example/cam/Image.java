48c69997208139922cf5fdddaea6a4791fbc9afe
jespinosa <jespinosa@neubus.com>
2013-11-01 17:36:40 -0500

    test

---
+++ src/com/example/cam/Image.java
@@ -0,0 +1,28 @@
+package com.example.cam;
+
+public class Image {
+	private long id;
+	private String name;
+	private String description;
+	
+	public long getId(){
+		return id;
+	}
+	public void setId(long id){
+		this.id = id;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getDescription(){
+		return description;
+	}
+	public void setDescription(String desc){
+		this.description = desc;
+	}
+	
+
+}
